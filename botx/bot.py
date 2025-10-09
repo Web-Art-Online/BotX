74cd254188ee73a4cdcc798e8d423ce7211c6b41
MrCarbene <66732742+sx120609@users.noreply.github.com>
2025-10-09 13:27:12 +0800

    识别中文井号

--- botx/bot.py
+++ botx/bot.py
@@ -41,9 +41,18 @@ class Bot:
     __online: bool
     __tasks: dict[str, dict]
 
-    def __init__(self, ws_uri: str, *, token: str | None = None, cmd_prefix: list[str] = ["#"], msg_cd: float = 0.1, log_level = "INFO"):
-        self.ws_uri = ws_uri
-        self.__token = token
+def __init__(
+    self, 
+    ws_uri: str, 
+    *, 
+    token: str | None = None, 
+    cmd_prefix: list[str] = ["#", "＃"], 
+    msg_cd: float = 0.1, 
+    log_level = "INFO"
+):
+    self.ws_uri = ws_uri
+    self.__token = token
+    self.cmd_prefix = cmd_prefix
 
         self.cmd_prefix = cmd_prefix
         self.me = None
